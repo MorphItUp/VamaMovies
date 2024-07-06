c439b4e0a0b34fefc470bc8656e861c68c13b6a1
Mohamed Elgedawy <me0123@dexcom.com>
2024-07-06 14:58:07 +0300

    Initial Commit

---
+++ VamaMovies/VamaMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VamaMovies
+//
+//  Created by Mohamed Elgedawy on 06/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
